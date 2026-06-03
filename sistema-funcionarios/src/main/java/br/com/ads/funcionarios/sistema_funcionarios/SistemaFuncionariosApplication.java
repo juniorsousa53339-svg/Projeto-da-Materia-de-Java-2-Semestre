bf6085517521b8c0ca5330eaffae681a9f569104
juniorsousa53339-svg <juniorsousa53339@gmail.com>
2026-06-03 14:20:46 -0300

    Primeira versão do backend

---
+++ sistema-funcionarios/src/main/java/br/com/ads/funcionarios/sistema_funcionarios/SistemaFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ads.funcionarios.sistema_funcionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFuncionariosApplication.class, args);
+	}
+
+}
---
+++ sistema-funcionarios/src/test/java/br/com/ads/funcionarios/sistema_funcionarios/SistemaFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ads.funcionarios.sistema_funcionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
